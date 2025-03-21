095f6c62da58950a02a1eb9139f696f90054ef0b
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2025-03-21 09:43:08 +0545

    enqueue method created

--- part_4/queue.js
+++ part_4/queue.js
@@ -12,8 +12,23 @@ class Queue {
     this.last = newNode;
     this.length = 1;
   }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    }
+
+    this.last.next = newNode;
+    this.last = newNode;
+    this.length++;
+    return this;
+  }
 }
 
 const firstQueue = new Queue(0);
-
+firstQueue.enqueue(1);
+firstQueue.enqueue(2);
+firstQueue.enqueue(3);
 console.log("----", firstQueue);
