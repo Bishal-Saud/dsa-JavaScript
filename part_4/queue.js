13bc01857b4858c4e66624be3ef0ac093d3fe501
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2025-03-25 09:47:59 +0545

    dequeue- like pop method- created dequeue method

--- part_4/queue.js
+++ part_4/queue.js
@@ -25,10 +25,26 @@ class Queue {
     this.length++;
     return this;
   }
+  dequeue() {
+    let temp = this.first;
+
+    if (this.length === 0) return null;
+    if (this.length === 1) {
+      this.first = null;
+      this.last = null;
+    }
+    this.first = this.first.next;
+    temp.next = null;
+
+    this.length--;
+    return temp;
+  }
 }
 
 const firstQueue = new Queue(0);
 firstQueue.enqueue(1);
 firstQueue.enqueue(2);
 firstQueue.enqueue(3);
+console.log("----", firstQueue);
+firstQueue.dequeue();
 console.log("----", firstQueue);
