1cf7cf52ebf983495802a9be3e039ef4c9e9b739
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2025-03-30 11:04:47 +0545

    set method created - hashtanle

--- 05_hashTables/hashTable.js
+++ 05_hashTables/hashTable.js
@@ -3,16 +3,29 @@ class HashTable {
     this.keyMap = new Array(size);
   }
 
-  _hashFunction(keyMap) {
+  _hashFunction(key) {
     let sum = 0;
     const PRIME_NUMBER = 31;
 
-    for (let i = 0; i < Math.min(keyMap.length, 100); i++) {
-      const charCode = keyMap.charCodeAt(0) - 96;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      const charCode = key.charCodeAt(0) - 96;
 
       sum = (sum * PRIME_NUMBER + charCode) % this.keyMap.length;
     }
 
     return sum;
   }
+
+  set(key, value) {
+    const index = this._hashFunction(key);
+    console.log("-----", index);
+
+    if (!this.keyMap[index]) this.keyMap[index] = [];
+    this.keyMap[index].push([key, value]);
+    return this;
+  }
 }
+
+const phoneBook = new HashTable();
+
+console.log(phoneBook.set("john", "555-444-33"));
