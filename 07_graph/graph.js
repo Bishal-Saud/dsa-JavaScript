eda5210b41dc31adc2fff132ca8b7edf0192a28c
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2025-05-05 09:41:08 +0545

    Edge added

--- 07_graph/graph.js
+++ 07_graph/graph.js
@@ -10,10 +10,21 @@ class Graph {
     }
     return false;
   }
+
+  addEdge(vtx1, vtx2) {
+    if (this.adjacencyList[vtx1] && this.adjacencyList[vtx2]) {
+      this.adjacencyList[vtx1].push(vtx2);
+      this.adjacencyList[vtx2].push(vtx1);
+      return true;
+    }
+    return false;
+  }
 }
 
 const graph = new Graph();
 graph.addVertex("A");
 graph.addVertex("B");
 graph.addVertex("C");
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
 console.log(graph);
