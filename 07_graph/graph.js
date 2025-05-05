b527ef77e816f8e8732029f48e8e441ad8404fad
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2025-05-05 09:34:03 +0545

    vertexAdded

--- 07_graph/graph.js
+++ 07_graph/graph.js
@@ -2,8 +2,18 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
+  addVertex(vtx) {
+    if (!this.adjacencyList[vtx]) {
+      this.adjacencyList[vtx] = [];
+      return true;
+    }
+    return false;
+  }
 }
 
 const graph = new Graph();
-
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
 console.log(graph);
